eb457bd961e7385d705eeb6cada49effa77449f5
Rny8 <neongirl3@yahoo.com>
2022-08-05 13:03:51 -0500

    Added some features lol

--- home/home.js
+++ home/home.js
@@ -2,4 +2,45 @@ window.onload = (event) => {
     document.getElementById("nameHeader").style.animation="nameAppear 0.5s linear 1 forwards";
     document.getElementById("belowName").style.animation="nameAppear 0.5s linear 1 forwards";
     document.getElementById("learnMore").style.animation="nameAppear 0.5s linear 1 forwards";
+
+    document.getElementById("learnMore").onclick = function() {
+        document.getElementById("aboutmeInfo2").scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
+    }
+
+    window.addEventListener('scroll', function() {
+        if(window.scrollY > 100) {
+            document.getElementById("aboutMeContainer").style.animation="fadein 1s forwards";
+        }
+        console.log(window.scrollY)
+    });
+
+
+    window.addEventListener('scroll', function() {
+        if(window.scrollY > 1005) {
+            document.getElementById("project1").style.animation="fadein 2s forwards";
+        }
+        
+    });
+
+
+    centerBox = document.getElementById("project1image")
+
+    document.onmousemove = handleMouseMove;
+    function handleMouseMove(event) {
+        event = event || window.event;
+
+        sensitivity = 0.005;
+        
+        mouseX = event.clientX;
+        mouseY = event.clientY;
+
+        offsetX = mouseX * sensitivity;
+        offsetY = mouseY * sensitivity;
+
+        centerBox.style.transform = `translateX(-${offsetX}px) translateY(-${offsetY}px)`
+    }
+
+
+
+
 }
